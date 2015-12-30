0766ff8c553d951ef29ca81536b6fe25808cd1ab
giraffe0813 <yemengying1993@foxmail.com>
2015-12-30 17:20:34 +0800

    写代码要规范。。。。

--- geekbook.py
+++ geekbook.py
@@ -40,26 +40,26 @@ def getPdf(html):
 	for url in imglist:
 		print("https://www.geekbooks.me" + url)
 		file_name = url.split('/')[-1]
-    	u = opener.open("https://www.geekbooks.me" + url)
-    	print("preparing......")
-    	f = open(file_name, 'wb')
-    	meta = u.info()
-    	file_size = int(meta.getheaders("Content-Length")[0])
-    	print "Downloading: %s Bytes: %s" % (file_name, file_size)
-    	file_size_dl = 0
-    	block_sz = 8192
-    	while True:
-        	buffer = u.read(block_sz)
-        	if not buffer:
+		u = opener.open("https://www.geekbooks.me" + url)
+		print("preparing......")
+		f = open(file_name, 'wb')
+		meta = u.info()
+		file_size = int(meta.getheaders("Content-Length")[0])
+		print "Downloading: %s Bytes: %s" % (file_name, file_size)
+		file_size_dl = 0
+		block_sz = 8192
+		while True:
+			buffer = u.read(block_sz)
+			if not buffer:
 				break
 
-        	file_size_dl += len(buffer)
-        	f.write(buffer)
-        	status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        	status = status + chr(8)*(len(status)+1)
-        	print status,
+			file_size_dl += len(buffer)
+			f.write(buffer)
+			status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+			status = status + chr(8)*(len(status)+1)
+			print status,
 
-    	f.close()
+		f.close()
 
 #get page from specific url
 html = getHtml("https://www.geekbooks.me/book/view/angularjs-directives-cookbook")
