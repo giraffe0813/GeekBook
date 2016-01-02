f3acfb37a620f8af4eceb41afa0fba1faa9347ed
giraffe0813 <yemengying1993@foxmail.com>
2016-01-02 13:25:53 +0800

    remove damaged file

--- scripts/detector.py
+++ scripts/detector.py
@@ -1,16 +1,19 @@
 from pyPdf import PdfFileReader
 import glob
+import os
 
 if __name__ == "__main__":
-    f = open("./damaged.txt","a+");
+    f = open("./damaged.txt", "a+");
     books = glob.glob("./books/*/*.pdf")
     for book in books:
         # print book
         try:
             mypdf = PdfFileReader(file(book, 'rb'))
         except:
-            print 'invalid pdf >',book
-            #write to file
+
+            print 'invalid pdf >', book
+            os.remove(book)
+            # write to file
             f.write(book+'\n')
     f.close()
 
